3c2faa62fdb33b60de1f0d437150c7ea36c42098
Emmanuel Alanis <ealanisln@me.com>
2024-08-04 18:28:19 -0700

    :sparkles: Added structure

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Copia e impresión en blanco y negro",
+    description:
+      "Ofrecemos copias e impresiones en tamaños carta, oficio y doble carta, tanto para mayoreo como para menudeo. Calidad y rapidez en cada copia.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Impresión a color en diversos tamaños y materiales",
+    description:
+      "Imprime en tamaño carta, tabloide, y más, sobre papeles especiales como bond, adherible, cuche y opalina. Opciones para todas tus necesidades de impresión.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Empastado y engargolado profesional",
+    description:
+      "Protege y organiza tus documentos con nuestros servicios de empastado en pasta blanda o rígida, y engargolado en diferentes tamaños.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Servicio de enmicado rígido",
+    description:
+      "Conserva tus documentos importantes con nuestro servicio de enmicado, disponible en tamaños desde carta hasta doble carta.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Entrega a domicilio gratis en compras mayores a $999 MXN",
+    description:
+      "Aprovecha nuestra entrega a domicilio sin costo adicional en pedidos superiores a $1000. Comodidad y conveniencia en cada compra.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Descuentos por volumen y calidad garantizada",
+    description:
+      "Imprime grandes cantidades con nosotros y disfruta de descuentos por volumen. Calidad en cada impresión, garantizada.",
+  },
+];
+
+export default featuresData;
